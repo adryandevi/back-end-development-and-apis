5c384d478c95b1e300f001e77f4d73182452fa75
adryandevi <doggerman33@gmail.com>
2026-09-20 17:38:03 +0000

    File Processor

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
+const fs = require('fs'); 
 
 console.log(fs);
 
 const http = require("http");
+
+const data = fs.readFileSync("assets/poem.txt");
+console.log(data); // <Buffer 48 65 6c 6c 6f ...>
