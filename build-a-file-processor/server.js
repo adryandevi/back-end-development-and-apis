8a8778ac1b14ab079d6d728a5f896e42142fe6e6
adryandevi <doggerman33@gmail.com>
2026-09-20 17:55:39 +0000

    asdas

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,1 +1,7 @@
-// Starter file — add your code here
+const fs = require('fs');
+
+console.log(fs);
+
+const data = fs.readFileSync('assets/poem.txt');
+
+console.log(data);
--- server.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs'); 
-
-console.log(fs);
-
-const http = require("http");
-
-fs.readFile("path/to/file.txt", { encoding: "utf8" }, (err, data) => {
-  console.log(data);
-});
